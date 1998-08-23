ec1bf864b436ad51e1d960e83d83b4d9dc8471ec
akr <akr>
1998-08-23 19:14:33 +0000

    * ew-bq.el: Re-indented.

--- ew-bq.el
+++ ew-bq.el
@@ -78,38 +78,22 @@
   (append "!*+-/0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" ()))
 
 (defconst ew-ccl-256-to-64-table
-  '(nil  nil  nil  nil  nil  nil  nil  nil
-    nil  nil  nil  nil  nil  nil  nil  nil
-    nil  nil  nil  nil  nil  nil  nil  nil
-    nil  nil  nil  nil  nil  nil  nil  nil
-    nil  nil  nil  nil  nil  nil  nil  nil
-    nil  nil  nil   62  nil  nil  nil   63
-     52   53   54   55   56   57   58   59
-     60   61  nil  nil  nil    t  nil  nil
-    nil    0    1    2    3    4    5    6
-      7    8    9   10   11   12   13   14
-     15   16   17   18   19   20   21   22
-     23   24   25  nil  nil  nil  nil  nil
-    nil   26   27   28   29   30   31   32
-     33   34   35   36   37   38   39   40
-     41   42   43   44   45   46   47   48
-     49   50   51  nil  nil  nil  nil  nil
-    nil  nil  nil  nil  nil  nil  nil  nil
-    nil  nil  nil  nil  nil  nil  nil  nil
-    nil  nil  nil  nil  nil  nil  nil  nil
-    nil  nil  nil  nil  nil  nil  nil  nil
-    nil  nil  nil  nil  nil  nil  nil  nil
-    nil  nil  nil  nil  nil  nil  nil  nil
-    nil  nil  nil  nil  nil  nil  nil  nil
-    nil  nil  nil  nil  nil  nil  nil  nil
-    nil  nil  nil  nil  nil  nil  nil  nil
-    nil  nil  nil  nil  nil  nil  nil  nil
-    nil  nil  nil  nil  nil  nil  nil  nil
-    nil  nil  nil  nil  nil  nil  nil  nil
-    nil  nil  nil  nil  nil  nil  nil  nil
-    nil  nil  nil  nil  nil  nil  nil  nil
-    nil  nil  nil  nil  nil  nil  nil  nil
-    nil  nil  nil  nil  nil  nil  nil  nil))
+  '(nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil
+    nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil
+    nil nil nil nil nil nil nil nil nil nil nil  62 nil nil nil  63
+     52  53  54  55  56  57  58  59  60  61 nil nil nil   t nil nil
+    nil   0   1   2   3   4   5   6   7   8   9  10  11  12  13  14
+     15  16  17  18  19  20  21  22  23  24  25 nil nil nil nil nil
+    nil  26  27  28  29  30  31  32  33  34  35  36  37  38  39  40
+     41  42  43  44  45  46  47  48  49  50  51 nil nil nil nil nil
+    nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil
+    nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil
+    nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil
+    nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil
+    nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil
+    nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil
+    nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil
+    nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil nil))
 
 (defconst ew-ccl-64-to-256-table
   '(?A ?B ?C ?D ?E ?F ?G ?H ?I ?J ?K ?L ?M ?N ?O ?P
