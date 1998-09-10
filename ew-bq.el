2176ed7536e22b2b0166193827e6a4f7e72b7021
akr <akr>
1998-09-10 03:44:23 +0000

    * FLIM-ELS (flim-modules): Delete `ew-ccl'.
    * ew-bq.el: Always use mel.
    
    * ew-ccl.el: Abolished.

--- ew-bq.el
+++ ew-bq.el
@@ -1,25 +1,7 @@
 (require 'emu)
 (require 'mel)
 
-(defvar ew-bq-use-mel (not (fboundp 'make-ccl-coding-system)))
-
-(cond
- (ew-bq-use-mel
-  (defalias 'ew-decode-q 'q-encoding-decode-string)
-  (defalias 'ew-decode-b 'base64-decode-string))
- (t
-  (require 'ew-ccl)
-  (defun ew-decode-q (str)
-    (string-as-unibyte (decode-coding-string str 'ew-ccl-uq)))
-  (if base64-dl-module
-      (defalias 'ew-decode-b 'base64-decode-string)
-    (defun ew-decode-b (str)
-      (string-as-unibyte (decode-coding-string str 'ew-ccl-b))))))
+(defalias 'ew-decode-q 'q-encoding-decode-string)
+(defalias 'ew-decode-b 'base64-decode-string)
 
 (provide 'ew-bq)
-
-;;;
-(defvar ew-bq-use-mel nil)
-
-(defun ew-encode-uq (str)
-  (encode-coding-string (string-as-unibyte str) 'ew-ccl-uq))
