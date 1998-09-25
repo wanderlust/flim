4dc71362e0c75d346e4dd5a2a73da0164d83b7bc
akr <akr>
1998-09-25 05:07:40 +0000

    * ew-compat.el: New file.
    * FLIM-ELS (flim-modules): Add `ew-compat'.

---
+++ ew-compat.el
@@ -0,0 +1,25 @@
+(require 'ew-dec)
+(require 'eword-decode)
+
+(defun ew-gnus-structured-field-decoder (string)
+  (if (fboundp 'ew-decode-field)
+      (let ((ew-ignore-76bytes-limit t)
+	    (ew-default-mime-charset default-mime-charset))
+	(condition-case nil
+	    (ew-cut-cr-lf (ew-decode-field "From" (ew-lf-crlf-to-crlf string)))
+	  (error
+	   (message "gnus-structured-field-decoder error: %s" string)
+	   (decode-mime-charset-string string 'x-ctext))))
+    (eword-decode-and-unfold-structured-field string)))
+
+(defun ew-gnus-unstructured-field-decoder (string)
+  (if (fboundp 'ew-decode-field)
+      (let ((ew-ignore-76bytes-limit t)
+	    (ew-default-mime-charset default-mime-charset))
+	(condition-case nil
+	    (ew-cut-cr-lf (ew-decode-field "Subject" (ew-lf-crlf-to-crlf string)))
+	  (error
+	   (message "gnus-unstructured-field-decoder error: %s" string)
+	   (decode-mime-charset-string string 'x-ctext))))
+    (eword-decode-unstructured-field-body (std11-unfold-string string) 'must-unfold)))
+
