2f102b8cec69260034b90f53b0507536c0d0d200
morioka <morioka>
1998-10-24 14:34:33 +0000

    Don't check `base64-encode-string' is builtin or not.

--- mel.el
+++ mel.el
@@ -96,8 +96,7 @@ Content-Transfer-Encoding for it."
 
 (mel-define-backend "binary" ("8bit"))
 
-(when (and (fboundp 'base64-encode-string)
-	   (subrp (symbol-function 'base64-encode-string)))
+(when (fboundp 'base64-encode-string)
   (mel-define-backend "base64")
   (mel-define-method-function (mime-encode-string string (nil "base64"))
 			      'base64-encode-string)
