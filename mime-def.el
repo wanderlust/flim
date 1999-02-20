8a821bc1096b8e3d708add159a9c813d409ffea6
akr <akr>
1999-02-20 13:22:44 +0000

    * mime-def.el (mime-library-product): Bump up to FLAM-DOODLE 1.12.3.

--- mime-def.el
+++ mime-def.el
@@ -27,7 +27,7 @@
 (require 'mcharset)
 
 (eval-and-compile
-  (defconst mime-library-product ["FLAM-DOODLE" (1 12 2) "代赭 10R4.5/8.0"]
+  (defconst mime-library-product ["FLAM-DOODLE" (1 12 3) "焦茶 10R3.0/2.0"]
     "Product name, version number and code name of MIME-library package.")
   )
 
