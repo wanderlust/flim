842882c04052353e0fbc4bc136eecfdb802e7365
yamaoka <yamaoka>
1999-08-26 09:08:37 +0000

    (smtpmail-send-it): Use `time-stamp-yyyy-mm-dd' and `time-stamp-hh:mm:ss' instead of `current-time'.

--- smtpmail.el
+++ smtpmail.el
@@ -230,11 +230,10 @@ This is relative to `smtpmail-queue-dir'.")
 					  tembuf))
 		      (error "Sending failed; SMTP protocol error"))
 		(error "Sending failed; no recipients"))
-	    (let* ((file-data (concat smtpmail-queue-dir
-				      (mapconcat
-				       (function
-					(lambda (arg) (format "%x" arg)))
-				       (current-time) "")))
+	    (let* ((file-data (concat
+			       smtpmail-queue-dir
+			       (concat (time-stamp-yyyy-mm-dd)
+				       "_" (time-stamp-hh:mm:ss))))
 		   (file-elisp (concat file-data ".el"))
 		   (buffer-data (create-file-buffer file-data))
 		   (buffer-elisp (create-file-buffer file-elisp))
