bed25c668bdd9bb1d3af1c6ddc17331560bb4a6d
yamaoka <yamaoka>
2001-07-30 11:15:04 +0000

    Synch with flim-1_14 branch.

--- sasl.el
+++ sasl.el
@@ -37,14 +37,17 @@
 ;;; Code:
 
 (defvar sasl-mechanisms
-  '("CRAM-MD5" "DIGEST-MD5" "PLAIN" "LOGIN" "ANONYMOUS"))
+  '("CRAM-MD5" "DIGEST-MD5" "PLAIN" "LOGIN" "ANONYMOUS"
+    "NTLM" "SCRAM-MD5"))
 
 (defvar sasl-mechanism-alist
   '(("CRAM-MD5" sasl-cram)
     ("DIGEST-MD5" sasl-digest)
     ("PLAIN" sasl-plain)
     ("LOGIN" sasl-login)
-    ("ANONYMOUS" sasl-anonymous)))
+    ("ANONYMOUS" sasl-anonymous)
+    ("NTLM" sasl-ntlm)
+    ("SCRAM-MD5" sasl-scram)))
 
 (defvar sasl-unique-id-function (function sasl-unique-id-function))
 
