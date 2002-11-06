26f42977cc1f7d01522fd7a30c12df6dcd8bc459
yamaoka <yamaoka>
2002-11-06 06:34:04 +0000

    Synch with the flim-1_14 branch.

--- mime-parse.el
+++ mime-parse.el
@@ -496,6 +496,7 @@ If Content-Transfer-Encoding field is not found, return nil."
 (defun mime-parse-buffer (&optional buffer representation-type)
   "Parse BUFFER as a MIME message.
 If buffer is omitted, it parses current-buffer."
+  (require 'mmbuffer)
   (save-excursion
     (if buffer (set-buffer buffer))
     (mime-parse-message (or representation-type
