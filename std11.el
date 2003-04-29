c47fbcbb2e121d38100257f79575670be8a37560
yamaoka <yamaoka>
2003-04-29 22:42:16 +0000

    Synch to the flim-1_14 branch.

--- std11.el
+++ std11.el
@@ -46,9 +46,7 @@ The optional argument BOUNDs the search; it is a buffer position."
     (if (re-search-forward "^$" bound t)
 	(goto-char (1- (match-beginning 0)))
       (end-of-line)
-      ))
-  (point)
-  )
+      (point))))
 
 ;;;###autoload
 (defun std11-fetch-field (name)
