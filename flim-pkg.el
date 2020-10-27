52d86b126bff77aa77d4883f70a05c1bc690953c
Kazuhiro Ito <kzhr@d1.dion.ne.jp>
2020-10-27 21:34:28 +0900

    Update package description file for MELPA.
    * flim-pkg.el: Define supported Emacs version.  Add dependency on
    oauth2.el.  It is required by sasl-xoauth2.el.

--- flim-pkg.el
+++ flim-pkg.el
@@ -1,3 +1,5 @@
 (define-package "flim" "1.14.9"
   "A library to provide basic features about message representation or encoding."
-  '((apel "10.8")))
+  '((emacs "24.5")
+    (apel "10.8")
+    (oauth2 "0.11")))
